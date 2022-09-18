192e55c36bdddc34151526f5a2e10e9d53ed1d12
valde <valde@LAPTOP-VV5M2GS7>
2022-09-17 21:55:01 -0500

    Agregar archivos de proyecto.

---
+++ U1Act1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using U1Act1.Models;
+
+namespace U1Act1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MiPerfil(PerfilViewModel perfil)
+        {
+            return View(perfil);
+        }
+    }
+}
---
+++ U1Act1/Models/PerfilViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace U1Act1.Models
+{
+    public class PerfilViewModel
+    {
+        public string? Nombre { get; set; }
+        public int Semestre { get; set; }
+        public string? Periodo { get; set; }
+        public string? Materia { get; set; }
+        
+    }
+}
